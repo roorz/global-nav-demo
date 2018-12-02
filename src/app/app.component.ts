89979f4d3b195fb83883b955605b52b85143b01b
Ruaridh Hutchison <ruaridh@scotstonhill.com>
2018-12-02 16:40:21 +0000

    Added global menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'global-nav-demo';
+
+  ngOnInit() {
+    /*$('.nav-link').on('click', function() {
+      $('#example1').collapse('hide');
+      $('#example2').collapse('hide');
+      $('#example3').collapse('hide');
+      $('.glob-menu-icon').removeClass('active');
+    });
+
+    $('.glob-menu-icon').on('click', function () {
+      $('.glob-menu-icon').removeClass('active');
+      $(this).addClass('active');
+    });*/
+  }
 }
